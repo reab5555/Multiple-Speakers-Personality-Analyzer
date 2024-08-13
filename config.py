3af48dd0c6f7d358b5996e6c4e9a11128e0e6d5e
reab5555 <130560722+reab5555@users.noreply.github.com>
2024-08-13 16:44:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()  # This loads the variables from .env file
 
-openai_api_key = os.environ.get("openai_api_key")
-aws_access_key_id = os.environ.get("aws_access_key_id")
-aws_secret_access_key = os.environ.get("aws_secret_access_key")
+openai_api_key = "openai_api_key"
+aws_access_key_id = "aws_access_key_id"
+aws_secret_access_key = "aws_secret_access_key"
